0776903d105df5792baf080b2c6d463088d9e8e7
shr0ukgomaa33 <shroukgomaa8787@gmail.com>
2026-09-04 01:58:52 +0300

    Add project files.

---
+++ Advanced 3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
